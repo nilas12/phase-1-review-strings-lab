3d9bfb87e62825a1356a00b5597cfdcb882df428
Your Name <senasezer@sena-MacBook-Air.local>
2022-10-17 14:28:32 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Nilas"
+const welcomeMessage =  "Welcome to Flatbook, Nilas !"
+const excitedWelcomeMessage =  "WELCOME TO FLATBOOK, NILAS !"
+const shortGreeting =  "Welcome, N !"
